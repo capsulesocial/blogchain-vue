76eaced2fe9168ea60eed4735741cc1be9197176
jackdishman <me@jackdishman.com>
2022-07-29 09:19:14 -0400

    feat: separate widgets into page-specific components

--- src/components/WidgetsContainer.vue
+++ src/components/WidgetsContainer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SideNav from '@/components/SideNav.vue'
+import HomeWidgets from '@/components/widgets/HomeWidgets.vue'
 import Footer from '@/components/Footer.vue'
 import Ressouces from '@/components/Ressources.vue'
 </script>
@@ -8,10 +9,8 @@ import Ressouces from '@/components/Ressources.vue'
 		class="w-5/12 -mr-5 -mt-4 hidden overflow-y-auto p-4 lg:block"
 		:style="`min-height: calc(100vh - 150px); height: calc(100vh - 80px)`"
 	>
-		<SideNav
-			class="from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border-lightBorder mb-5 overflow-hidden rounded-lg border bg-gradient-to-r shadow-lg"
-			style="backdrop-filter: blur(10px)"
-		/>
+		<HomeWidgets v-if="$route.name === `Home`" />
+		<SideNav />
 		<Ressouces />
 		<Footer />
 	</aside>
