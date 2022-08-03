499da33f4e831a0bb8756817330ced1555964b74
jackdishman <me@jackdishman.com>
2022-08-03 11:49:13 -0400

    feat: init tag card

---
+++ src/components/TagCard.vue
@@ -0,0 +1,17 @@
+<template>
+	<button
+		class="focus:outline-none bg-gray1 dark:bg-gray7 z-10 transform rounded-lg px-3 pb-1 transition duration-500 ease-in-out"
+		@click="$router.push('/tag/' + tag)"
+	>
+		<span class="text-gray5 dark:text-gray1 text-sm"> {{ tag }} </span>
+	</button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	tag: {
+		type: String,
+		required: true,
+	},
+})
+</script>
--- src/components/widgets/TagsWidget.vue
+++ src/components/widgets/TagsWidget.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TagCard from '@/components/TagCard.vue'
+// TODO: fetch tags from store / backend
+const tags = ref<string[]>([`test`])
+</script>
 <template>
-	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg">Tags</div>
+	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg">
+		<div class="px-6 pb-4">
+			<p v-if="tags.length === 0" class="text-gray5 dark:text-gray3 mb-4 mt-3 text-sm">
+				<span> It seems no trending tags are available at the moment </span>
+			</p>
+			<TagCard v-for="t in tags" v-else :key="t" :tag="t" class="my-2 mr-4 text-lg" />
+		</div>
+	</div>
 </template>
