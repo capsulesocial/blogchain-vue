d0f786310d35aab67a2b4503026e270e914e3f13
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-08-04 13:30:02 +0200

    clean widgets

--- src/components/widgets/FollowersWidget.vue
+++ src/components/widgets/FollowersWidget.vue
@@ -40,11 +40,11 @@ const followers = ref<Profile[]>([
 <template>
 	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg px-6 py-4">
 		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText text-base font-semibold mb-2">Recent Followers</h3>
-		<div class="">
+		<div>
 			<p v-if="followers.length === 0" class="text-gray5 dark:text-gray3 mb-4 mt-3 text-sm">
 				<span> It seems no one is following you yet </span>
 			</p>
-			<HorizontalProfilePreview v-for="profile in followers" :key="profile.id" :profile="profile" class="" />
+			<HorizontalProfilePreview v-for="profile in followers" :key="profile.id" :profile="profile" />
 			<button class="text-primary text-sm mt-2" @click="$emit(``)">Show more</button>
 		</div>
 	</div>
