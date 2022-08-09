69deb85ddabd8c69a7a20a3037db8ab989f32e96
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-08-09 17:09:52 +0200

    fix side bar padding and content heights

--- src/components/WidgetsContainer.vue
+++ src/components/WidgetsContainer.vue
@@ -3,10 +3,7 @@ import HomeWidgets from '@/components/widgets/HomeWidgets.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 <template>
-	<aside
-		class="w-5/12 -mr-5 -mt-4 hidden overflow-y-auto p-4 lg:block"
-		:style="`min-height: calc(100vh - 150px); height: calc(100vh - 80px)`"
-	>
+	<aside class="w-5/12 -mr-5 -mt-4 hidden overflow-y-auto p-4 pb-32 lg:block min-h-61 h-61">
 		<HomeWidgets v-if="$route.name === `Home`" />
 		<Footer />
 	</aside>
