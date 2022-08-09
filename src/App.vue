64deea4c3271a33797922ebc1e58ecebcd614ee1
Irshad PI <irshadpi77@gmail.com>
2022-08-09 15:39:48 +0530

    feat: tryout change

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,6 @@ onBeforeMount(() => {
 // 	if (!store.$state.id) {
 // })
 </script>
-
 <template>
 	<metainfo>
 		<template #title="{ content }">{{ `${content}` }}</template>
@@ -53,31 +52,88 @@ onBeforeMount(() => {
 
 	<!-- Full screen pages -->
 	<router-view v-if="unauthRoutes.includes($route.name as string)" :key="$route.path" />
-	<!-- wrapper -->
-	<main
-		v-else
-		class="bg-img m-0 h-screen overflow-y-hidden p-0 bg-lightBG dark:bg-darkBG"
+
+	<div
+		class="w-full sticky top-0 h-38 z-50 bg-img bg-lightBG dark:bg-darkBG"
 		:style="{
 			backgroundImage: `url(` + getBGImage(store.background) + `)`,
 		}"
 	>
-		<!-- Wrapper -->
-		<div class="flex w-full justify-center">
-			<div class="flex flex-col w-full lg:w-11/12 xl:w-10/12 max-w-1220">
-				<TopHeader :name="store.name" :avatar="avatar" />
-				<!-- Body -->
-				<TitleContainer v-if="routesWithTitle.includes($route.name as string)" />
-				<section class="modal-animation flex flex-row lg:mt-2 xl:mt-5">
-					<div
-						:class="fullPageRoutes.includes($route.name as string) ? `w-full` : `lg:w-7.5`"
-						class="min-h-80 h-80 from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border border-lightBorder z-10 w-full overflow-y-auto rounded-t-lg bg-gradient-to-r shadow-lg"
-					>
-						<router-view :key="$route.path" />
-					</div>
-					<WidgetsContainer v-if="!fullPageRoutes.includes($route.name as string)" />
-				</section>
+		<div class="m-auto self-center w-full lg:w-11/12 xl:w-10/12 max-w-1220">
+			<TopHeader :name="store.name" :avatar="avatar" />
+			<!-- Body -->
+			<TitleContainer v-if="routesWithTitle.includes($route.name as string)" />
+		</div>
+	</div>
+	<!-- wrapper -->
+	<main
+		class="bg-img h-screen p-0 bg-lightBG dark:bg-darkBG justify-center flex"
+		:style="{
+			backgroundImage: `url(` + getBGImage(store.background) + `)`,
+		}"
+	>
+		<div
+			:class="fullPageRoutes.includes($route.name as string) ? `w-full` : `lg:w-7.5`"
+			class="scroll-wrapper min-h-80 h-80 from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border border-lightBorder z-10 w-full rounded-t-lg bg-gradient-to-r shadow-lg"
+		>
+			<div class="scroll-outer">
+				<div class="scroll-inner">
+					<router-view :key="$route.path" />
+				</div>
 			</div>
 		</div>
+		<!-- <div
+			:class="fullPageRoutes.includes($route.name as string) ? `w-full` : `lg:w-7.5`"
+			class="min-h-80 h-80 from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border border-lightBorder z-10 w-full rounded-t-lg bg-gradient-to-r shadow-lg"
+		></div> -->
+		<!-- <div :class="fullPageRoutes.includes($route.name as string) ? `w-full` : `lg:w-7.5`" class="relative">
+			<div class="flex w-full justify-center">
+				<div class="flex flex-col w-full lg:w-11/12 xl:w-10/12 max-w-1220">
+					<section class="modal-animation flex flex-row lg:mt-2 xl:mt-5">
+						<div
+							class="min-h-80 h-80 from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border border-lightBorder z-10 w-full rounded-t-lg bg-gradient-to-r shadow-lg"
+						>
+							<router-view :key="$route.path" />
+						</div>
+					</section>
+				</div>
+				<WidgetsContainer v-if="!fullPageRoutes.includes($route.name as string)" />
+			</div>
+		</div> -->
 	</main>
 	<div id="popup"></div>
 </template>
+<style>
+.scroll-wrapper {
+	position: static;
+}
+.scroll-outer {
+	/* overflow-y: auto; */
+	/* height: 100%; */
+}
+.scroll-inner {
+	font-weight: bold;
+	margin-left: auto;
+	margin-right: auto;
+	margin: 50px;
+	position: relative;
+	z-index: 1;
+}
+.scroll-wrapper:before,
+.scroll-wrapper:after {
+	height: 50px;
+	z-index: 2;
+	position: absolute;
+	left: 0;
+	right: 20px;
+	content: '';
+	display: block;
+}
+
+.scroll-wrapper:after {
+	bottom: 0;
+}
+.scroll-wrapper:before {
+	top: 0;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -82,4 +82,5 @@ onMounted(() => {
 			</div>
 		</div>
 	</nav>
+	<div v-for="i in 20" :key="i" class="w-full h-20 bg-gray1 rounded-lg my-2 overflow-y-auto"></div>
 </template>
