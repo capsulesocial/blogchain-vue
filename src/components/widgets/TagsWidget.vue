cf3c6bfd8232803976aa9eee9f285592643297db
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-08-11 15:39:30 +0200

    Add tag widget on Discover with image

--- src/components/widgets/TagsWidget.vue
+++ src/components/widgets/TagsWidget.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useStoreSettings } from '@/store/settings';
 import TagCard from '@/components/TagCard.vue';
+
+const settings = useStoreSettings();
+
 // TODO: fetch tags from store / backend
 const tags = ref<string[]>([`test`]);
 </script>
 <template>
-	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg px-6 py-4">
-		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText text-base font-semibold mb-2">Trending topics</h3>
-		<div class="">
+	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg">
+		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText text-base font-semibold mb-2 px-6 pt-4">
+			Trending topics
+		</h3>
+		<div class="px-6 pb-4">
 			<p v-if="tags.length === 0" class="text-gray5 dark:text-gray3 mb-4 mt-3 text-sm">
 				<span> It seems no trending topics are available at the moment </span>
 			</p>
 			<TagCard v-for="t in tags" v-else :key="t" :tag="t" class="my-2 mr-4 text-lg" />
 		</div>
+		<img
+			v-if="$route.name === `Discover`"
+			loading="lazy"
+			class="rounded-lg"
+			:src="
+				settings.isDarkMode
+					? require(`@/assets/images/brand/dark/discover.webp`)
+					: require(`@/assets/images/brand/light/discover.webp`)
+			"
+		/>
 	</div>
 </template>
