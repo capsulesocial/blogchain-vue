5ca84b3d501b07647ae8e8913a03c7ea04c30605
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-08-11 15:02:23 +0200

    hmoe bookmark widget

---
+++ src/components/HorizontalBookmarkPreview.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import { ref } from 'vue';
+import { Post } from '@/backend/post';
+
+defineProps({
+	bookmark: {
+		type: Object as PropType<Post>,
+		required: true,
+	},
+});
+
+const cid = ref<string>(``);
+const featuredPhoto = ref<any>();
+const authorName = ref<string>(``);
+
+// IF featuredPhotoCID: fetch featuredPhoto
+</script>
+
+<template>
+	<router-link :to="`/post/` + cid" class="flex w-full flex-row items-center my-4">
+		<!-- Left side: title and author name -->
+		<div class="flex flex-grow flex-col">
+			<h5 class="font-semibold dark:text-darkSecondaryText">{{ bookmark.title }}</h5>
+			<h6 v-if="authorName !== ``" class="text-gray5 dark:text-gray3">By {{ authorName }}</h6>
+			<h6 v-else class="text-gray5 dark:text-gray3">By @{{ bookmark.authorID }}</h6>
+		</div>
+		<!-- Right side: featured photo -->
+		<div class="w-24 hidden xl:block">
+			<img
+				v-if="featuredPhoto !== null"
+				:src="featuredPhoto"
+				:alt="bookmark.title"
+				class="h-16 w-full flex-shrink-0 rounded-lg object-cover"
+			/>
+		</div>
+	</router-link>
+</template>
--- src/components/widgets/BookmarksWidget.vue
+++ src/components/widgets/BookmarksWidget.vue
@@ -1,3 +1,62 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useStore } from '@/store/session';
+import { Post } from '@/backend/post';
+import HorizontalBookmarkPreview from '@/components/HorizontalBookmarkPreview.vue';
+
+const store = useStore();
+
+// TODO: fetch followers from store / backend
+const bookmarks = ref<Post[]>([
+	{
+		authorID: `johndoe`,
+		title: `First Bookmark`,
+		subtitle: ``,
+		content: ``,
+		featuredPhotoCID: null,
+		featuredPhotoCaption: null,
+		tags: [],
+		category: ``,
+		timestamp: 0,
+		postImages: [],
+		encrypted: false,
+	},
+	{
+		authorID: `johndoe`,
+		title: `Second Bookmark`,
+		subtitle: ``,
+		content: ``,
+		featuredPhotoCID: null,
+		featuredPhotoCaption: null,
+		tags: [],
+		category: ``,
+		timestamp: 1,
+		postImages: [],
+		encrypted: false,
+	},
+]);
+</script>
+
 <template>
-	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg">Bookmarks</div>
+	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg px-6 py-4 relative">
+		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText text-base font-semibold">Recent Bookmarks</h3>
+		<!-- Loading spinner -->
+		<div v-if="store.$state.id !== ``">
+			<div>
+				<article v-if="bookmarks.length == 0">
+					<p class="text-gray5 dark:text-gray3 mb-4 text-sm">
+						<span>
+							You haven't bookmarked any posts yet. Click the bookmark icon on a post to add it to your bookmark list
+						</span>
+					</p>
+				</article>
+				<HorizontalBookmarkPreview v-for="bookmark in bookmarks" :key="bookmark.timestamp" :bookmark="bookmark" />
+				<button class="text-primary text-sm">Show more</button>
+			</div>
+		</div>
+		<div v-else class="text-gray5 dark:text-gray3 text-sm px-6 pb-4">
+			<button class="text-primary focus:outline-none ml-1" @click="$router.push(`/register`)">Sign up</button>
+			to save bookmarks
+		</div>
+	</div>
 </template>
