c122262d87369ba1af6520e113ddf87bb98f9ded
John Philip <developerphilo@gmail.com>
2022-08-18 17:35:07 +0300

    fix: discover page

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps({
+	text: {
+		type: String,
+		required: true,
+	},
+	bgImage: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<article class="flex justify-center">
+		<router-link
+			v-if="bgImage !== ``"
+			:to="'/discover/' + text"
+			class="bg-primary flex w-full transform items-end overflow-hidden rounded-lg shadow-lg transition duration-500 ease-in-out hover:scale-105"
+			style="height: 200px"
+		>
+			<img
+				v-if="bgImage !== ``"
+				:src="require(`@/assets/images/category/` + bgImage + '/' + bgImage + `.webp`)"
+				:alt="text"
+				class="absolute left-0 top-0 object-cover w-full rounded-lg"
+				loading="lazy"
+				style="height: 200px"
+			/>
+			<div
+				class="absolute z-10 left-0 top-0 object-contain w-full h-full rounded-lg"
+				:style="{
+					background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%)`,
+					backgroundSize: 'cover',
+				}"
+			></div>
+			<div class="flex flex-col p-4 font-sans">
+				<h4 class="z-20 text-2xl font-extrabold capitalize text-white">
+					{{ text.replace(`-`, ` `) }}
+				</h4>
+			</div>
+		</router-link>
+	</article>
+</template>
--- src/pages/Discover/Index.vue
+++ src/pages/Discover/Index.vue
@@ -1,3 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import { categories } from '@/config/config';
+import CategoryCard from '@/components/CategoryCard.vue';
+import { getTags } from '@/backend/post';
 
-<template>Discover</template>
+const categoryList = ref<string[]>(categories);
+const tags = ref<string[]>([]);
+const showFilter = ref<boolean>(false);
+
+function handleDropdown(e: any): void {
+	if (!e.target || e.target.parentNode === null || e.target.parentNode.classList === undefined) {
+		return;
+	}
+	if (!e.target.parentNode.classList.contains(`toggle`)) {
+		showFilter.value = false;
+	}
+}
+
+onMounted(async () => {
+	const content = await getTags();
+	tags.value = content.slice(0, 14);
+	window.addEventListener(`click`, handleDropdown, false);
+});
+
+onUnmounted(() => {
+	window.removeEventListener(`click`, handleDropdown);
+});
+</script>
+
+<template>
+	<div class="min-h-61 h-61 xl:min-h-160 xl:h-160 border-lightBorder w-full border">
+		<div class="px-5 pb-2 pt-3 xl:px-6 xl:pt-4">
+			<h2 class="text-lightPrimaryText dark:text-darkPrimaryText mb-1 text-lg font-semibold xl:text-xl">
+				Featured Categories
+			</h2>
+			<p class="text-gray5 dark:text-gray3">Read more of what you love by browsing top categories:</p>
+		</div>
+		<article
+			class="min-h-170 h-170 xl:min-h-240 xl:h-240 box-border grid w-full grid-cols-1 gap-4 overflow-y-auto px-6 pb-8 pt-4 xl:grid-cols-2"
+		>
+			<CategoryCard v-for="c in categoryList" :key="c" :text="c" :bg-image="c" />
+		</article>
+	</div>
+</template>
