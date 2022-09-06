6a3f2291d7b035cb822c97a3b24ae24e73721634
jackdishman <me@jackdishman.com>
2022-09-06 12:26:34 -0400

    feat: implement posts on profile page with comments

--- src/pages/Profile/Posts.vue
+++ src/pages/Profile/Posts.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { useMeta } from 'vue-meta';
-import Comment from '@/components/post/Comment.vue';
+import PostCardContainer from '@/components/post/PostCardContainer.vue';
+import { storeToRefs } from 'pinia';
+import { usePostsStore } from '@/store/posts';
+
 useMeta({
 	title: `authorName - Blogchain`,
 	htmlAttrs: { lang: 'en', amp: true },
 });
+
+const postsStore = usePostsStore();
+const posts = storeToRefs(postsStore).posts;
 </script>
 
 <template>
@@ -12,6 +18,8 @@ useMeta({
 		id="scrollable_content"
 		class="min-h-115 h-115 lg:min-h-210 lg:h-210 xl:min-h-220 xl:h-220 overflow-y-auto lg:overflow-y-hidden relative w-full px-4"
 	>
-		<div v-for="i in 50" :key="i"><Comment class="mb-4" /></div>
+		<div v-for="post in posts" :key="`new_${post.post._id}`">
+			<PostCardContainer :fetched-post="post" />
+		</div>
 	</div>
 </template>
