9fe757f1145246c754f623510c619528b548c075
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-09-07 16:53:49 +0200

    base for comment template (#80)
    * base for comment template
    
    * feat: implement posts on profile page with comments
    
    * add replies to comments
    
    * update style of comments
    
    * fix: use router link instead of anchor tag
    
    * styling adjustments
    
    Co-authored-by: jackdishman <me@jackdishman.com>

--- src/pages/Profile/Posts.vue
+++ src/pages/Profile/Posts.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMeta } from 'vue-meta';
+import PostCardContainer from '@/components/post/PostCardContainer.vue';
+import { storeToRefs } from 'pinia';
+import { usePostsStore } from '@/store/posts';
+
+useMeta({
+	title: `authorName - Blogchain`,
+	htmlAttrs: { lang: 'en', amp: true },
+});
+
+const postsStore = usePostsStore();
+const posts = storeToRefs(postsStore).posts;
+</script>
 
 <template>
-	<div id="scrollable_content">{{ $route.params }}/posts</div>
+	<div
+		id="scrollable_content"
+		class="min-h-115 h-115 lg:min-h-210 lg:h-210 xl:min-h-220 xl:h-220 overflow-y-auto lg:overflow-y-hidden relative w-full"
+	>
+		<div v-for="post in posts" :key="`new_${post.post._id}`">
+			<PostCardContainer :fetched-post="post" />
+		</div>
+	</div>
 </template>
