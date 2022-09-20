e6f6d797b14dc414f996adcef951a28724a624cf
jackdishman <me@jackdishman.com>
2022-09-20 13:14:27 +0200

    fix: change array to set

--- src/components/widgets/FollowersWidget.vue
+++ src/components/widgets/FollowersWidget.vue
@@ -9,14 +9,15 @@ const followers = useConnectionsStore().followers;
 
 const recentFollowers = ref<Profile[]>([]);
 
-onBeforeMount(async () => {
+onBeforeMount(() => {
 	// Fetch (at most) two most recent followers
-	for (let i = 0; i < 2; i++) {
-		const f = await getProfile(followers[i]);
-		if (f.profile) {
-			recentFollowers.value.push(f.profile);
+	followers.forEach(async (f) => {
+		const res = await getProfile(f);
+		if (res.profile === null) {
+			return;
 		}
-	}
+		recentFollowers.value.push(res.profile);
+	});
 });
 
 const openFollowersPopup = ref<boolean>(false);
@@ -25,7 +26,7 @@ const openFollowersPopup = ref<boolean>(false);
 	<div class="bg-lightBG dark:bg-darkBGStop mb-5 rounded-lg border border-lightBorder shadow-lg px-6 py-4">
 		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText text-base font-semibold mb-2">Recent Followers</h3>
 		<div>
-			<p v-if="followers.length === 0" class="text-gray5 dark:text-gray3 mb-4 mt-3 text-sm">
+			<p v-if="followers.size === 0" class="text-gray5 dark:text-gray3 mb-4 mt-3 text-sm">
 				<span> It seems no one is following you yet </span>
 			</p>
 			<HorizontalProfilePreview v-for="profile in recentFollowers" :key="profile.id" :profile="profile" />
