992eabc2ea6b71f485b9e02fbb460f726c310381
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-09-29 10:49:20 +0200

    add category selection widget

--- src/components/widgets/pages/EditorWidgets.vue
+++ src/components/widgets/pages/EditorWidgets.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import UploadFeaturedPhotoWidget from '../UploadFeaturedPhotoWidget.vue';
+import UploadFeaturedPhotoWidget from '@/components/widgets/UploadFeaturedPhotoWidget.vue';
+import CategorySelectionWidget from '@/components/widgets/CategorySelectionWidget.vue';
 </script>
 <template>
 	<UploadFeaturedPhotoWidget />
+	<CategorySelectionWidget />
 </template>
