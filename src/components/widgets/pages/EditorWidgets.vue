e1d04b141347a6e3a81763516bae51faf26407ce
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-09-29 11:07:12 +0200

    add premium post toggle widget

--- src/components/widgets/pages/EditorWidgets.vue
+++ src/components/widgets/pages/EditorWidgets.vue
@@ -2,9 +2,11 @@
 import UploadFeaturedPhotoWidget from '@/components/widgets/UploadFeaturedPhotoWidget.vue';
 import CategorySelectionWidget from '@/components/widgets/CategorySelectionWidget.vue';
 import TagsSelectionWidget from '@/components/widgets/TagsSelectionWidget.vue';
+import PremiumPostToggleWidget from '../PremiumPostToggleWidget.vue';
 </script>
 <template>
 	<UploadFeaturedPhotoWidget />
 	<CategorySelectionWidget />
 	<TagsSelectionWidget />
+	<PremiumPostToggleWidget />
 </template>
