4ac2d7d97c5954782ec01906e7e0fd41d33633f4
John Philip <developerphilo@gmail.com>
2022-09-29 15:15:00 +0300

    fix: add friend button + profile cards

---
+++ src/components/post/ProfileCardHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+withDefaults(
+	defineProps<{
+		authorID: string;
+		authorName: string;
+		isHoverCard?: boolean;
+	}>(),
+	{
+		isHoverCard: false,
+	},
+);
+</script>
+<template>
+	<router-link :to="'/id/' + authorID" class="mr-4 flex" :class="isHoverCard ? `flex-col` : ``">
+		<span
+			v-if="authorName != ``"
+			class="text-base dark:text-darkPrimaryText transition ease-in-out hover:underline"
+			:class="isHoverCard ? `font-bold` : `font-medium`"
+		>
+			{{ authorName }}
+		</span>
+		<span
+			v-else
+			class="text-gray5 dark:text-gray3 text-base transition ease-in-out hover:underline"
+			:class="isHoverCard ? `font-bold` : `font-medium`"
+		>
+			{{ authorID }}
+		</span>
+		<span class="text-gray5 dark:text-gray3" :class="isHoverCard ? `` : `ml-2`"> @{{ authorID }} </span>
+	</router-link>
+</template>
