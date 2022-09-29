094d0a7634b63b8e717049a79b4ef49267dd78a4
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-09-29 10:55:45 +0200

    add tag selection widget

--- src/components/widgets/pages/EditorWidgets.vue
+++ src/components/widgets/pages/EditorWidgets.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import UploadFeaturedPhotoWidget from '@/components/widgets/UploadFeaturedPhotoWidget.vue';
 import CategorySelectionWidget from '@/components/widgets/CategorySelectionWidget.vue';
+import TagsSelectionWidget from '@/components/widgets/TagsSelectionWidget.vue';
 </script>
 <template>
 	<UploadFeaturedPhotoWidget />
 	<CategorySelectionWidget />
+	<TagsSelectionWidget />
 </template>
