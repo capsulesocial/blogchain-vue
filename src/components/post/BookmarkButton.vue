d63f5a00ee335541d373a82a4f68f8c5c6fb9e47
jackdishman <me@jackdishman.com>
2022-09-30 11:33:12 +0200

    feat: toggle bookmark status

--- src/components/post/BookmarkButton.vue
+++ src/components/post/BookmarkButton.vue
@@ -1,20 +1,41 @@
 <script setup lang="ts">
 import BookmarkIcon from '@/components/icons/BookmarkIcon.vue';
-withDefaults(
+import { ref } from 'vue';
+import { usePostsStore } from '@/store/posts';
+import { useStore } from '@/store/session';
+import { toastError, toastSuccess } from '@/plugins/toast';
+const props = withDefaults(
 	defineProps<{
 		hasBookmark: boolean;
 		postcid: string;
 	}>(),
 	{},
 );
+const postsStore = usePostsStore();
+const store = useStore();
+const isBookmarked = ref<boolean>(props.hasBookmark);
+const handleBookmark = async () => {
+	try {
+		await postsStore.handleBookmark(!isBookmarked.value, store.$state.id, props.postcid);
+	} catch (err) {
+		toastError(err as string);
+	} finally {
+		isBookmarked.value = !isBookmarked.value;
+		if (isBookmarked.value) {
+			toastSuccess(`You have added this post to your bookmarks`);
+			return;
+		}
+		toastSuccess(`You have removed this post from your bookmarks`);
+	}
+};
 </script>
 
 <template>
 	<button
 		class="focus:outline-none text-gray5 dark:text-gray3 hover:text-primary dark:hover:text-primary w-5 h-5"
 		style="margin-top: 2px"
-		@click="() => {}"
+		@click="handleBookmark"
 	>
-		<BookmarkIcon :is-active="hasBookmark" />
+		<BookmarkIcon :is-active="isBookmarked" />
 	</button>
 </template>
