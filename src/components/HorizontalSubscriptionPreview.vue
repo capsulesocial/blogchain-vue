8d363b2e807c5fbce753078604a61a7508ecd5ab
John Philip <developerphilo@gmail.com>
2022-10-05 19:04:06 +0300

    fix: allow subscription from expired subscriptions profiles (#142)

--- src/components/HorizontalSubscriptionPreview.vue
+++ src/components/HorizontalSubscriptionPreview.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-import Avatar from '@/components/Avatar.vue';
-import SubscribeButton from '@/components/subscriptions/SubscribeButton.vue';
+import { ref, computed, onMounted } from 'vue';
+import { useProfilesStore } from '@/store/profiles';
 import { ISubscriptionWithProfile } from '@/store/subscriptions';
 
-withDefaults(
+import Avatar from '@/components/Avatar.vue';
+import SubscribeButton from '@/components/subscriptions/SubscribeButton.vue';
+import SubscriptionsPopup from '@/components/popups/SubscriptionsPopup.vue';
+
+const profilesStore = useProfilesStore();
+
+const props = withDefaults(
 	defineProps<{
 		subscription: ISubscriptionWithProfile;
 	}>(),
 	{},
 );
+
+const showSubscription = ref<boolean>(false);
+const profile = computed(() => profilesStore.getProfile(props.subscription.authorID));
+
+onMounted(async (): Promise<void> => {
+	void profilesStore.fetchProfile(props.subscription.authorID);
+});
 </script>
 
 <template>
@@ -16,21 +29,37 @@ withDefaults(
 		<div class="flex flex-row">
 			<!-- Avatar -->
 			<Avatar
-				:cid="subscription.avatar"
-				:authorid="subscription.authorID"
+				:cid="props.subscription.avatar"
+				:authorid="props.subscription.authorID"
 				:size="`w-12 h-12 xl:w-14 xl:h-14`"
 				class="self-center"
 			/>
 			<!-- Middle: name and id -->
 			<div class="ml-4 self-center">
-				<h5 v-if="subscription.name !== ``" class="text-lightPrimaryText dark:text-darkPrimaryText">
-					{{ subscription.name }}
+				<h5 v-if="props.subscription.name !== ``" class="text-lightPrimaryText dark:text-darkPrimaryText">
+					{{ props.subscription.name }}
 				</h5>
-				<h5 v-else class="text-gray5 dark:text-gray3">{{ subscription.authorID }}</h5>
-				<h6 class="text-primary text-sm">@{{ subscription.authorID }}</h6>
+				<h5 v-else class="text-gray5 dark:text-gray3">
+					{{ props.subscription.authorID }}
+				</h5>
+				<h6 class="text-primary text-sm">@{{ props.subscription.authorID }}</h6>
 			</div>
 		</div>
 		<!-- Renew button -->
-		<SubscribeButton class="self-center" :is-subscribed="false" />
+		<SubscribeButton
+			class="self-center"
+			:is-subscribed="false"
+			:action="() => (showSubscription = !showSubscription)"
+		/>
 	</div>
+	<Teleport to="body">
+		<SubscriptionsPopup
+			v-if="showSubscription"
+			:is-subscribed="false"
+			:author="profile"
+			:author-avatar="profile.avatar"
+			:enabled-tiers="[]"
+			@close="showSubscription = false"
+		/>
+	</Teleport>
 </template>
