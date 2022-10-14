de41431fc7797dd7c31c719acff81b7ab52710f2
John Philip <developerphilo@gmail.com>
2022-10-14 14:40:06 +0300

    fix: subscriptions loading screen (#181)
    * fix: subscriptions loading screen
    
    * fix: move to mounted
    
    Co-authored-by: jackdishman <me@jackdishman.com>

--- src/pages/Subscriptions.vue
+++ src/pages/Subscriptions.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useMeta } from 'vue-meta';
 import { useStore } from '@/store/session';
 import { useStoreSettings } from '@/store/settings';
 import { useSubscriptionStore, ISubscriptionWithProfile } from '@/store/subscriptions';
+
 import SubscriptionPreview from '@/components/subscriptions/SubscriptionPreview.vue';
 import SubInfosPopup from '@/components/popups/SubInfosPopup.vue';
 import ChangeTierPopup from '@/components/popups/ChangeTierPopup.vue';
-import SpinnerIcon from '@/components/icons/SpinnerIcon.vue';
 import { createDefaultProfile } from '@/backend/profile';
+
 const isLoading = ref(true);
 const store = useStore();
 const settings = useStoreSettings();
 const subStore = useSubscriptionStore();
-subStore.fetchSubs(store.$state.id);
-isLoading.value = false;
 
-const subscriptions = ref<ISubscriptionWithProfile[]>([]);
+const subscriptions = computed(() => subStore.$state.active);
 const toggleSubInfoPopup = ref(false);
 const subscriptionInfo = ref();
 const showChangeTier = ref(false);
 const subscriptionProfile = ref(createDefaultProfile(store.$state.id));
 const subscriptionProfileAvatar = ref<string | undefined>();
 const authorPaymentProfile = ref<ISubscriptionWithProfile | undefined>();
-
-subscriptions.value = subStore.$state.active;
 
 useMeta({
 	title: `Active Subscriptions - Blogchain`,
@@ -42,60 +39,65 @@ function toggleChangeTierPopup(author: { sub: ISubscriptionWithProfile; avatar: 
 	authorPaymentProfile.value = author.sub;
 	showChangeTier.value = !showChangeTier.value;
 }
+
+onMounted(async () => {
+	await subStore.fetchSubs(store.$state.id);
+	isLoading.value = false;
+});
 </script>
 <template>
-	<div>
-		<div class="px-5 pt-3 pb-3 xl:px-6 xl:pt-4">
-			<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-lg font-semibold xl:text-xl">My subscriptions</h2>
-			<p class="text-gray5 dark:text-gray3">Here you can manage your active subscriptions to your favorite authors</p>
+	<div class="px-5 pt-3 pb-3 xl:px-6 xl:pt-4">
+		<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-lg font-semibold xl:text-xl">My subscriptions</h2>
+		<p class="text-gray5 dark:text-gray3">Here you can manage your active subscriptions to your favorite authors</p>
+	</div>
+	<article
+		id="scrollable_content"
+		class="min-h-115 h-115 w-full overflow-y-auto px-5 sm:px-4 xl:px-5 pt-2 lg:overflow-y-hidden relative"
+	>
+		<!-- loading spinner -->
+		<div v-if="isLoading" class="modal-animation flex w-full justify-center z-20 mt-24">
+			<div
+				class="loader m-5 border-2 border-gray1 dark:border-gray7 h-8 w-8 rounded-3xl"
+				:style="`border-top: 2px solid`"
+			></div>
 		</div>
-		<article
-			id="scrollable_content"
-			class="min-h-115 h-115 lg:min-h-210 lg:h-210 xl:min-h-220 xl:h-220 w-full overflow-y-auto px-5 sm:px-4 xl:px-5 pt-2 lg:overflow-y-hidden relative"
-		>
-			<div v-if="subscriptions.length > 0 && store.$state.id !== ``" class="flex flex-wrap items-start">
-				<SubscriptionPreview
-					v-for="subscription in subscriptions"
-					:key="subscription.subscriptionId"
-					:subscription="subscription"
-					@sub-info-popup="showSubInfoPopup(subscription)"
+		<!-- show subscriptions -->
+		<div v-if="!isLoading && subscriptions.length > 0 && store.$state.id !== ``" class="flex flex-wrap items-start">
+			<SubscriptionPreview
+				v-for="subscription in subscriptions"
+				:key="subscription.subscriptionId"
+				:subscription="subscription"
+				@sub-info-popup="showSubInfoPopup(subscription)"
+			/>
+		</div>
+		<!-- no subscriptions -->
+		<div v-if="!isLoading && store.$state.id !== `` && subscriptions.length <= 0">
+			<div class="flex flex-col items-center">
+				<p class="text-gray5 dark:text-gray3 align-end mb-1 mt-6 flex items-end text-sm w-3/4 text-center">
+					It seems like you don't currently have any active subscriptions. Browse Blogchain and subscribe to authors to
+					view them here
+				</p>
+				<img
+					:src="
+						require(settings.isDarkMode
+							? `@/assets/images/brand/dark/subscriptions.webp`
+							: `@/assets/images/brand/light/subscriptions.webp`)
+					"
+					class="h-auto"
+					loading="lazy"
 				/>
 			</div>
-			<div v-if="isLoading" class="flex items-center justify-center py-20">
-				<SpinnerIcon class="w-6 h-6" />
-			</div>
-			<div v-if="subscriptions.length <= 0 && store.$state.id !== ``">
-				<div class="flex flex-col items-center">
-					<p class="text-gray5 dark:text-gray3 align-end mb-1 mt-6 flex items-end text-sm w-3/4 text-center">
-						It seems like you don't currently have any active subscriptions. Browse Blogchain and subscribe to authors
-						to view them here
-					</p>
-					<img
-						:src="
-							require(settings.isDarkMode
-								? `@/assets/images/brand/dark/subscriptions.webp`
-								: `@/assets/images/brand/light/subscriptions.webp`)
-						"
-						class="h-auto"
-						loading="lazy"
-					/>
-				</div>
-			</div>
-		</article>
-		<div v-if="toggleSubInfoPopup">
-			<SubInfosPopup
-				:sub="subscriptionInfo"
-				@switch-popup="toggleChangeTierPopup"
-				@close="toggleSubInfoPopup = false"
-			/>
 		</div>
-		<div v-if="showChangeTier && authorPaymentProfile">
-			<ChangeTierPopup
-				:author="subscriptionProfile"
-				:sub="authorPaymentProfile"
-				:author-avatar="subscriptionProfileAvatar"
-				@close="showChangeTier = false"
-			/>
-		</div>
+	</article>
+	<div v-if="toggleSubInfoPopup">
+		<SubInfosPopup :sub="subscriptionInfo" @switch-popup="toggleChangeTierPopup" @close="toggleSubInfoPopup = false" />
+	</div>
+	<div v-if="showChangeTier && authorPaymentProfile">
+		<ChangeTierPopup
+			:author="subscriptionProfile"
+			:sub="authorPaymentProfile"
+			:author-avatar="subscriptionProfileAvatar"
+			@close="showChangeTier = false"
+		/>
 	</div>
 </template>
