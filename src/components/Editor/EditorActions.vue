030a9e5825a206c12e6b21e11112bc15db59e5f3
John Philip <developerphilo@gmail.com>
2022-10-25 11:23:17 +0300

    fix: correctly emit event

--- src/components/Editor/EditorActions.vue
+++ src/components/Editor/EditorActions.vue
@@ -47,7 +47,7 @@ onBeforeMount(() => {
 			style="left: 50px; top: -12px"
 		>
 			<!-- Add Image -->
-			<button class="focus:outline-none text-gray5 flex hover:text-lightPrimaryText" @click="emit(`image`)">
+			<button class="focus:outline-none text-gray5 flex hover:text-lightPrimaryText" @click="$emit(`image`)">
 				<ImageIcon class="h-5 w-5 fill-current" />
 			</button>
 		</div>
